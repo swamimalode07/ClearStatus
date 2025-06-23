5342a2b6c505ef92cd1132aa562c3c6c1e2c0c31
swamimalode07 <swamimalode2004@gmail.com>
2025-06-24 02:33:45 +0530

    added frontend base

--- status-frontend/src/middleware.ts
+++ status-frontend/src/middleware.ts
@@ -1,9 +1,10 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  publicRoutes: ["/", "/sign-in(.*)", "/sign-up(.*)"],
-});
+export default clerkMiddleware();
 
 export const config = {
+  // Specify the public routes here
   matcher: ["/((?!_next|.*\\..*).*)"],
+  // Correct place to define public routes
+  publicRoutes: ["/", "/sign-in(.*)", "/sign-up(.*)"],
 };
